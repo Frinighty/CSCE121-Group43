3adf56af0ba41006fed1223e1812cde8aad5de7f
Frinighty <Fssunesara@gmail.com>
2015-04-09 15:13:56 -0500

    Create drawpancakes.cpp
    Create pancakes based on number of pancakes needed for selected difficulty. Might need to move some parts to game portion in order to attach it to the gameplay scene.

---
+++ drawpancakes.cpp
@@ -0,0 +1,25 @@
+#include Graph.h
+#include Window.h
+using namespace std;
+//initialize number of pancakes
+class pancake {
+int width;
+int position;
+};
+pancake::pancake (int a, int b) {
+    pancake_width = a*50
+    pancake_position = b*50
+    Rectangle good_pancake(Point(100,pancake_position),pancake_width,50)
+    good_pancake.set_fill_color(fl_rgb_color(232,217,175))
+    Rectangle burnt_pancake(Point(100,pancake_position+50),pancake_width,50)
+    burnt_pancake.set_fill_color(fl_rgb_color(153,102,51))
+}
+
+int main() {
+int number_of_pancakes;
+vector <pancake> pancakes;
+cin >> number_of_pancakes;
+for (i=0,i<number_of_pancakes,++i)
+    pancake pancake1 (i,i);
+    pancake1.push_back();
+}
